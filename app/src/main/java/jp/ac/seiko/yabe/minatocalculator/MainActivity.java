6207913c0e8eb5cebe8ab57c73819bf028d1bf5a
yabeminato <62219yabe@seiko.ac.jp>
2020-03-28 11:17:07 +0900

    //絶対動かない

--- app/src/main/java/jp/ac/seiko/yabe/minatocalculator/MainActivity.java
+++ app/src/main/java/jp/ac/seiko/yabe/minatocalculator/MainActivity.java
@@ -1,4 +1,5 @@
 package jp.ac.seiko.yabe.minatocalculator;
+//plz wait
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,46 +8,99 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-    private Button mButton1;
-    private Button mButton2;
-    private Button mButton3;
-    private Button mButton4;
-    private Button mButton5;
-    private Button mButton6;
-    private Button mButton7;
-    private Button mButton8;
-    private Button mButton9;
-    private Button mClear;
-    private Button mtasu;
-    private Button mhiku;
-    private Button mkakeru;
-    private Button mwaru;
-    private Button mikoulu;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button mButton[];
+
+    int mId[] = {
+            R.id.button2, R.id.button3, R.id.button6, R.id.button7, R.id.button8, R.id.button9, R.id.button11, R.id.button12, R.id.button13, R.id.button17, R.id.button_clear, R.id.button_hiku, R.id.button_ikoolu, R.id.button_kakeru, R.id.button_purasu, R.id.button_waru, R.id.lnput, R.id.answer
+    };
+    //定義　
+    private int PUSh_PLUS = 10;
+    private int PUSH_MINUS = 11;
+    private int PUSH_KAKERU = 12;
+    private int PUSH_WARU = 13;
+    private int PUSH_CLEAR = 14;
+    private int PUSH_IKOULU = 15;
     private TextView mAnswer;
     private TextView mInput;
+
+    private String[] answers = new String [4];
+
+    //始まり
+    private int Status = 0;
+
+    private ArrayList<String> calcArray;
+    private ArrayList<String> signArray;
+    //終わり
+     private int Final = 0;
+
+    //Fuck
+    //FUCK!!!!!!!!!!!!
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mButton1 = findViewById(R.id.button11);
-        mButton2 = findViewById(R.id.button12);
-        mButton3 = findViewById(R.id.button13);
-        mButton4 = findViewById(R.id.button3);
-        mButton5 = findViewById(R.id.button8);
-        mButton6 = findViewById(R.id.button9);
-        mButton7 = findViewById(R.id.button2);
-        mButton8 = findViewById(R.id.button6);
-        mButton9 = findViewById(R.id.button7);
-        mClear = findViewById(R.id.button_clear);
-        mtasu = findViewById(R.id.button_purasu);
-        mhiku = findViewById(R.id.button_hiku);
-        mkakeru = findViewById(R.id.button_kakeru);
-        mwaru = findViewById(R.id.button_waru);
-        mikoulu = findViewById(R.id.button_ikoolu);
         mAnswer = findViewById(R.id.answer);
         mInput= findViewById(R.id.lnput);
+        mButton = new Button[mId.length];
+
+        for (int i = 0; i < mId.length; i++) {
+            mButton[i] = findViewById(mId[i]);
+            mButton[i].setOnClickListener(this);
+        }
+        calcArray = new ArrayList<String>();
+        signArray = new ArrayList<String>();
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        //  判定　　
+        for (int i = 0; i < mId.length; i++){
+            if (v.equals(mButton[i])) {
+                String nowValue = mAnswer.getText().toString();
+                if (i == PUSH_CLEAR) {
+                    mAnswer.setText("");
+                    //進行度
+                    calcArray.clear();
+                    signArray.clear();
+                    Status = PUSH_CLEAR;
+                }
+
+                else if (i == PUSh_PLUS && nowValue.length() > 0) {
+                    calcArray.add(nowValue);
+                    signArray.add("+");
+                    Status = PUSh_PLUS;
+                }
+                else if (i == PUSH_MINUS && nowValue.length() > 0) {
+                    calcArray.add(nowValue);
+                    signArray.add("-");
+                    Status = PUSH_MINUS;
+                }
+                else if (i == PUSH_KAKERU && nowValue.length() > 0) {
+                    calcArray.add(nowValue);
+                    signArray.add("×");
+                    Status = PUSH_KAKERU;
+                }
+                else if (i == PUSH_WARU && nowValue.length() > 0) {
+                    calcArray.add(nowValue);
+                    signArray.add("÷");
+                }
+                else if (i == PUSH_IKOULU && nowValue.length() > 0) {
+                    //To Do
+                }
+                else if (i < 10) {
+                    nowValue = nowValue + i;
+                    mAnswer.setText(nowValue);
+                    Status = i;
+                }
+                break;
+            }
+        }
+
     }
 
 
